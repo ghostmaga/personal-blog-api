3601001ef0d78e5484a1061eeec378272ece24eb
ghostmaga <magzhan.khorshat@bk.ru>
2025-03-20 23:45:10 +0500

    del

--- test.py
+++
@@ -1,7 +0,0 @@
-﻿import environ, os
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '/.env'))
-print(env('DB_HOST'))    
